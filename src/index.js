76cdd8e11c6def1e8b82995d8b2435544a142320
Efrain Rodriguez <efrainrg92@gmail.com>
2020-03-22 19:13:47 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+//requerimentos
+const express = require('express');
+const path = require('path');
+//const http = require('http');
+
+const app = express();
+//const server = http.createServer(app);
+
+//configurações
+app.set('port', 3000);
+
+//recursos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+//rotas
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+//servidor ouvindo...
+app.listen(app.get('port'), ()=>{
+    console.log('servidor no porto', app.get('port'));
+});
